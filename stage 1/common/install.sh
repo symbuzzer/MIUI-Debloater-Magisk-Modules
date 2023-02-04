a5f40b5f8f794daaaae033da6729e47b33882ece
Ali Beyaz <symbuzzer@users.noreply.github.com>
2023-02-04 22:30:56 +0300

    Update install.sh

---
+++ stage 1/common/install.sh
@@ -0,0 +1,18 @@
+ui_print "Updates will be uninstalled..."
+pm uninstall-system-updates com.miui.analytics
+pm uninstall-system-updates com.facebook.appmanager
+pm uninstall-system-updates com.xiaomi.joyose
+pm uninstall-system-updates com.miui.daemon
+pm uninstall-system-updates com.miui.msa.global
+pm uninstall-system-updates com.xiaomi.mipicks
+pm uninstall-system-updates com.xiaomi.payment
+pm uninstall-system-updates com.xiaomi.simactivate.service
+pm uninstall-system-updates com.mipay.wallet.in
+pm uninstall-system-updates com.facebook.system
+pm uninstall-system-updates com.facebook.services
+pm uninstall-system-updates com.mi.android.globalminusscreen
+pm uninstall-system-updates com.miui.yellowpage
+pm uninstall-system-updates com.google.android.apps.subscriptions.red
+pm uninstall-system-updates com.mipay.android.manager
+pm uninstall-system-updates com.tencent.soter.soterserver
+pm uninstall-system-updates org.ifaa.aidl.manager
